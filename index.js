f4ddbdeb252a90911fa42b0f9d1feb5daffea17b
kaneez <k.fatima.dev@gmail.com>
2024-02-14 08:35:40 -0800

    add spinner and toggle button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let container = document.getElementById("container");
 let input = document.getElementById("input");
 let searchBtn = document.getElementById("button-addon2");
+let spinner = document.getElementById("spinner");
 
 let func = async () => {
   try {
@@ -10,7 +11,8 @@ let func = async () => {
     let data = await response.json();
     let recipe = data.hits;
     console.log(data);
-    display(recipe); 
+    hidespinner();
+    display(recipe);
   } catch (error) {
     console.error("Error fetching data:", error);
   }
@@ -19,20 +21,19 @@ let func = async () => {
 func();
 
 let fetchSearch = async () => {
- 
   try {
-    let url =
-      `https://api.edamam.com/search?q=${input.value}&app_id=eaa27b60&app_key=f02d46b15e42e56c3137f24194783f50&from=0&to=100`;
+    let url = `https://api.edamam.com/search?q=${input.value}&app_id=eaa27b60&app_key=f02d46b15e42e56c3137f24194783f50&from=0&to=100`;
     let response = await fetch(url);
     let data = await response.json();
     let recipe = data.hits;
     console.log(data);
-    container.innerHTML=" "
-    display(recipe); 
+    hidespinner();
+    container.innerHTML = "";
+    display(recipe);
   } catch (error) {
     console.error("Error fetching data:", error);
   }
-  input.value = ""
+  input.value = "";
 };
 
 let display = (recipe) => {
@@ -42,33 +43,47 @@ let display = (recipe) => {
     container.appendChild(msg);
   }
 
-  recipe.forEach((recipeItem) => {
+  recipe.forEach((recipeItem, i) => {
     let output = document.createElement("div");
-    output.setAttribute("class","d-inline-flex col-lg-4 col-md-6 col-12 my-4 justify-content-center "
+    output.setAttribute(
+      "class",
+      "d-inline-flex col-lg-4 col-md-6 col-12 my-4 justify-content-center "
     );
     output.innerHTML += `
       <div class="card" style="width: 16rem;">
       <img src="${recipeItem.recipe.image}" class="card-img-top" alt="... ">
         <div class="card-body">
-       <h6 class="card-text">${recipeItem.recipe.label}</small></h6>
-       <p class="card-text">${recipeItem.recipe.cuisineType}</small></h6>
-          <p class="card-title d-none text-start ingd " id="ingd">${recipeItem.recipe.ingredientLines
-            .map((x) => x)
-            .join(" ")}</p>
-          <div class="btn btn-primary my-2"onclick="show()">Show Ingredient</div>
+       <h5 class="card-text">${recipeItem.recipe.label}</small></h6>       
+       ${recipeItem.recipe.cuisineType
+         .map(
+           (cuisine) =>
+             `<p>Dish Type :<strong> ${
+               cuisine.charAt(0).toUpperCase() + cuisine.slice(1)
+             }</strong></p>`
+         )
+         .join("")}
+            <p class="ingd d-none" id="ingd${i}">
+            ${recipeItem.recipe.ingredientLines
+              .map(
+                (x, index, array) =>
+                  `${x}${index !== array.length - 1 ? "," : ""}`
+              )
+              .join(" ")}
+          </p>
+          <div class="btn btn-primary my-2" onclick="show(${i})">Show Ingredient</div>
           <a href=${
             recipeItem.recipe.url
           } target="_blank" class="btn btn-secondary mx-auto">Get Recipe</a>
         </div>
       </div>`;
     container.appendChild(output);
-   
   });
 };
-
+// show ingredients
 const show = (recipeItem) => {
-console.log(recipeItem)
+  document.getElementById("ingd" + recipeItem).classList.toggle("d-none");
 };
-
-
-
+//hide loading spinner
+const hidespinner = () => {
+  spinner.style.display = "none";
+};
